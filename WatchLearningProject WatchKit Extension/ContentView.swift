e05a8fd95f1a3a0afc4b842d90087e905329e025
Afina R. Vinci <afinarv@gmail.com>
2022-07-19 19:05:57 +0700

    Initial commit - finished project

--- WatchLearningProject WatchKit Extension/ContentView.swift
+++ WatchLearningProject WatchKit Extension/ContentView.swift
@@ -6,11 +6,32 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
+    @StateObject var dataManager = DataManager()
+    let activities: [(name: String, type: HKWorkoutActivityType)] = [("Cycling", .cycling), ("Running", .running), ("Wheelchair", .wheelchairRunPace)]
+    @State private var selectedActivity = 0
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        if dataManager.state == .inactive {
+            VStack {
+                
+                Picker("Choose an activity", selection: $selectedActivity) {
+                    ForEach(0..<activities.count) { index in
+                        Text(activities[index].name)
+                    }
+                }
+                Button("Start Workout") {
+                    guard HKHealthStore.isHealthDataAvailable() else { return }
+                    dataManager.activity = activities[selectedActivity].type
+                    dataManager.start()
+                }
+            }
+            
+        } else {
+            WorkoutView(dataManager: dataManager)
+        }
     }
 }
 
